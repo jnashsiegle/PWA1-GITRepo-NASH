00f4a3b475aaa27c6ccb0bf57d796814513aa482
jnashsiegle <jsiegle@Janas-MacBook-Pro.local>
2014-11-11 22:57:35 -0600

    Duel - wk3
    COMPLETELY FINISHED!!  YAY!
    Just need to do flowcharts and pseudocode before turning in

--- Wk3/goal1_duel1/js/main.js
+++ Wk3/goal1_duel1/js/main.js
@@ -41,33 +41,31 @@ window.onload = (function() {
         }];
 
 
-    var round = 0;
+    var round = 1;
 
     //Initialize DOM's innerHTML text for top of page
 
     round_txt.innerHTML = "Click Fight Button to Start!";
     playerMac_txt.innerHTML = fighters[0].name + ":  " + fighters[0].health;
     playerWindows_txt.innerHTML = fighters[1].name + ":  " + fighters[1].health;
+	 
+	 
+		 
+	
 console.log(playerWindows_txt.innerHTML = fighters[1].name + ":  " + fighters[1].health, playerMac_txt.innerHTML = fighters[0].name + ":  " + fighters[0].health, "both should be at 100");
+
+    //why is the above console outputting 100 health correctly but the DOM is outputting health after the first round
+
 
     function fight() {                                                  //need a function calculating the fight
         //console.log("In the fight function");                           //prints to console that the fight FN is
         //printing off
-        //alert(fighters[0].name + ":" + fighters[0].health + " *START* " + fighters[1].name + ":" + fighters[1].health);
-
+       // alert(fighters[0].name + ":" + fighters[0].health + " *START* " + fighters[1].name + ":" + fighters[1].health);
 
         playerMac_txt.innerHTML = fighters[0].name + ":  " + fighters[0].health;
         playerWindows_txt.innerHTML = fighters[1].name + ":  " + fighters[1].health;
-        console.log("fight starts, what is the health?" + fighters[0].name + ":  " + fighters[0].health, fighters[1].name + ":  " + fighters[1].health);
-
-        /*//reconfigured alert reflecting array
-         //alert for player to start of game
-         //for (var i = 0; i < 10; i++) {                                      //formula for random number damage to hit player
-         //Random formula is - Math.floor(random() * (max - min) + min);
-
-         var minDamage1 = fighters[0] * .5;                    //minimal dmg to player one / reconfigured for array
-         var minDamage2 = fighters[1] * .5;                //minimal dmg to player two / reconfigured for array*/
-
+		
+       // console.log("fight starts, what is the health?  " + fighters[0].name + ":  " + fighters[0].health, fighters[1].name + ":  " + fighters[1].health);
 
         var f1 = Math.floor(Math.random() * fighters[0].damage + fighters[0].damage * .5);  //dmg formula to players
         var f2 = Math.floor(Math.random() * fighters[1].damage + fighters[1].damage * .5);  //each players health
@@ -81,12 +79,12 @@ console.log(playerWindows_txt.innerHTML = fighters[1].name + ":  " + fighters[1]
         fighters[0].health -= f1;                //deducts the damage from players health / reconfigured to reflect array
         fighters[1].health -= f2;        //deducts the damage from the players health / reconfigured to reflect array
 
-        console.log(fighters[0].name + ":" + fighters[0].health + " " + fighters[1].name + ":" + fighters[1].health);
+        //console.log(fighters[0].name + ":" + fighters[0].health + " " + fighters[1].name + ":" + fighters[1].health);
         //this will print out to console the player's
         //name and health with each round / //reconfigured to reflect array
 
         var result = winnerCheck();
-        console.log(result);
+        //console.log(result);
 
         round_txt.innerHTML = "Round #" + round + " Results:";
         round++;
@@ -108,14 +106,14 @@ console.log(playerWindows_txt.innerHTML = fighters[1].name + ":  " + fighters[1]
 
 
         function winnerCheck() {
-            console.log("are we getting to the winner check?");
+            //console.log("are we getting to the winner check?");
             var result = "no winner";
             if (fighters[0].health < 0 && fighters[1].health < 0) {
                 result = "You Both Die - GAME OVER";
             } else if (fighters[0].health < 1) {
-                result = fighters[1].name + "WINS!!";
+                result = fighters[1].name + " WINS!!";
             } else if (fighters[1].health < 1) {
-                result = fighters[0].name + "WINS!!";
+                result = fighters[0].name + " WINS!!";
             }
             return result;
 
@@ -123,7 +121,7 @@ console.log(playerWindows_txt.innerHTML = fighters[1].name + ":  " + fighters[1]
     /**This is where the program starts to run**/                   //Start of program
     console.log("Program Starts");                                   //prints showing program starts
 
-    fight();                                                            //calling or invoking the fight FN
+   // fight();                                                            //calling or invoking the fight FN
 
 
 })();                                                                  //end of program
