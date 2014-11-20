4f35cac3219eb9e0e260dd0b0bfb1bb29ca38132
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2014-11-20 11:25:10 -0600

    Basic Code down for Final Programming
    Time to start troubleshooting and make it work

--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -1,5 +1,54 @@
 /**
- * Created by the JavaScript Development Team
- * Class: PWA
+ * Jana Nash-Siegle
  * Goal: Goal7
+ * Final
  */
+(function(){
+
+window.Person = Person;    //Adding the person object to the global window for main.js
+
+
+    Person.jobs = ["Fireman", "Grocer", "Programmer", "Musician"];   //creating jobs for the people
+    Person.actions = ["Working Out", "Shopping", "Sleeping", "Eating", "Reading", "Working"];  //activities for the people
+
+    function Person(name, row){                         //FN to create the person
+        console.log("I created a person by the name of:  ", name);
+
+        this.name = name;                   //enables HTML page positioning
+
+        this.row = row;                     //enables HTML page positioning
+
+        this.job = Person.jobs[Math.floor[Math.random() + Person.actions.length]]; //Persons starting position
+
+        this.action = Person.actions[Math.floor[Math.random() + Person.actions.length]];  //how the person will start
+
+
+        var id = document.getElementById("r" + this.row + "c3");   //what are we going to begin displaying and where??
+        id.innerHTML = this.action;
+
+    }
+
+
+
+    Person.prototype.update = function(){
+
+        if(Math.floor[Math.random()] <.01){
+
+            var i  = Math.floor[Math.random() + Person.actions.length];
+            this.action = Person.actions[i];
+            id.innerHTML = this.action;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+})();
