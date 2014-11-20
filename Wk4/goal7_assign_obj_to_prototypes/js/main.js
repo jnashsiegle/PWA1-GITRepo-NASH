6d38478130aaaf88d84d8bf6cf79861bc6ed271a
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2014-11-20 16:32:16 -0600

    Progress so far of Final
    Works in Chrome, Safari but not Firefox  or Firefox Developer :(  Still
    have to do auto update

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -14,28 +14,35 @@
 
     for (var i = 0; i < numPeople; i++) {               //FOR loop to cycle through names
 
-        var personIndex = Math.floor[Math.random() + names.length];     //this is going to create our # to go no higher
+        var personIndex = Math.floor(Math.random() * names.length);     //this is going to create our # to go no higher
                                                                         //than 7 *# of names*
         var person = new Person(names[personIndex], i + 1);    //setting up the new person object
 
-        populateHTML(person.name, "r1", + (i + 1) + "c1");       //call to populate person's name and where on HTML
-        populateHTML(person.job, "r1", + ( i + 1) + "c2");       //call to populate person's job and where on HTML
+        populateHTML(person.name, "r" + (i + 1) + "c1");       //call to populate person's name and where on HTML
+        populateHTML(person.job, "r" + ( i + 1) + "c2");       //call to populate person's job and where on HTML
+
 
         people.push(person);
         names.splice(personIndex, 1);
 
-        clearInterval(interval);                    //one at a time and clear it out on renew
-
-        interval = setInterval(runUpdate, 1000/30);  //setting interval to 30; will continue to run til clear is called
-                                                    //or we shut window ; ID value is for interval parameter for clear
 
         function populateHTML (field, data){
 
             var id = document.getElementById(data);
-            id.innerHTML = data;
+            id.innerHTML = (field);
 
         }
+        /*interval = setInterval(runUpdate, 1000/30);  //setting interval to 30; will continue to run til clear is called
+                                                    //or we shut window ; ID value is for interval parameter for clear
+*/
+        clearInterval(interval);                    //one at a time and clear it out on renew
 
+
+
+
+
+
+/*
         function runUpdate(){
             people.forEach(function(Person){
                 //console.log(name)
@@ -43,7 +50,7 @@
                 //.update = the prototype method in the person.js file
                 Person.update();
             });
-        }
+        } */
     }
 
 
--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -8,8 +8,8 @@
 window.Person = Person;    //Adding the person object to the global window for main.js
 
 
-    Person.jobs = ["Fireman", "Grocer", "Programmer", "Musician"];   //creating jobs for the people
-    Person.actions = ["Working Out", "Shopping", "Sleeping", "Eating", "Reading", "Working"];  //activities for the people
+    var jobs = ["Fireman", "Grocer", "Programmer", "Musician"];   //creating jobs for the people
+    var actions = ["Working Out", "Shopping", "Sleeping", "Eating", "Reading", "Working"];  //activities for the people
 
     function Person(name, row){                         //FN to create the person
         console.log("I created a person by the name of:  ", name);
@@ -18,9 +18,9 @@ window.Person = Person;    //Adding the person object to the global window for m
 
         this.row = row;                     //enables HTML page positioning
 
-        this.job = Person.jobs[Math.floor[Math.random() + Person.actions.length]]; //Persons starting position
+        this.job = jobs[Math.floor(Math.random() * jobs.length)]; //Persons starting position
 
-        this.action = Person.actions[Math.floor[Math.random() + Person.actions.length]];  //how the person will start
+        this.action = actions[Math.floor(Math.random() * actions.length)];  //how the person will start
 
 
         var id = document.getElementById("r" + this.row + "c3");   //what are we going to begin displaying and where??
@@ -30,16 +30,16 @@ window.Person = Person;    //Adding the person object to the global window for m
 
 
 
-    Person.prototype.update = function(){
+    /* Person.prototype.update = function(){
 
         if(Math.floor[Math.random()] <.01){
 
-            var i  = Math.floor[Math.random() + Person.actions.length];
+            var i  = Math.floor[Math.random() * Person.actions.length];
             this.action = Person.actions[i];
             id.innerHTML = this.action;
         }
     }
-
+*/
 
 
 
